604c38fa136b0fdbc89cf6bb39f5fb067abe123d
noamessi07 <noakims@yahoo.com>
2021-12-22 21:43:37 -0500

    second change

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "myserver" {
     count                  = "2"
     ami                    = "ami-0ed9277fb7eb570c9"
     instance_type          = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.mysg-sg.id]
+    #vpc_security_group_ids = [aws_security_group.mysg-sg.id]
 
     tags = {
         name = "testserver"
