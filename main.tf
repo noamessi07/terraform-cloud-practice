7e7f112963ffb7cd810a43c07a1c989afaeac55c
noamessi07 <noakims@yahoo.com>
2021-12-22 21:40:34 -0500

    first change

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-ressource "aws_instance" "myserver" {
+resource "aws_instance" "myserver" {
     count                  = "2"
     ami                    = "ami-0ed9277fb7eb570c9"
     instance_type          = "t2.micro"
