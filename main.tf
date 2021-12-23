b93fbc7e714e1a5d80b7be2f2aca854d52d1a2f7
noamessi07 <noakims@yahoo.com>
2021-12-22 23:08:33 -0500

    final change

--- main.tf
+++ main.tf
@@ -2,13 +2,9 @@ resource "aws_instance" "myserver" {
     count                  = "2"
     ami                    = "ami-0ed9277fb7eb570c9"
     instance_type          = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.mysg-sg.id]
+    #vpc_security_group_ids = [aws_security_group.mysg-sg.id]
 
     tags = {
         name = "testserver"
     }
-}
-
-resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.0.0/16"
 }
